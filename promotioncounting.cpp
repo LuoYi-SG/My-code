c6f4d9bf82d8ef3041a43b9a017d5db2edbb9585
Luo Yi <luoyi501@gmail.com>
2022-01-20 21:03:13 +0800

    Create promotioncounting.cpp

---
+++ promotioncounting.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ifstream fin ("promote.in");
+    ofstream fout ("promote.out");
+    int before [4], after [4];
+    for (int i = 0; i < 4; i ++) {
+        fin >> before [i] >> after [i];
+    }
+    int increase [3] = { 0 };
+    increase [2] = after [3] - before [3];
+    increase [1] = after [2] + increase [2] - before [2];
+    increase [0] = after [1] + increase [1] - before [1];
+    fout << increase [0] << "\n" << increase [1] << "\n" << increase [2] << "\n";
+}
