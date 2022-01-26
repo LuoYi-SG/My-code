8697d58ccd61a0966b84efc0506bfc4f8844ad14
Luo Yi <luoyi501@gmail.com>
2022-01-26 21:50:09 +0800

    Uploaded
    Ignore the file name. It was my second draft.

---
+++ cowtipii.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ifstream fin ("cowtip.in");
+    ofstream fout ("cowtip.out");
+    int N;
+    fin >> N;
+    bool cowstatus [10] [10];
+    for (int i = 0; i < N; i ++) {
+        char temp [N];
+        fin >> temp;
+        for (int j = 0; j < N; j ++) {
+            cowstatus [i] [j] = temp [j] - '0';
+        }
+    }
+    int ans = 0;
+    for (int x = N - 1; x >= 0; x --) {
+        for (int y = N - 1; y >= 0; y --) {
+            if (cowstatus [x] [y] == 1) {
+                for (int i = 0; i <= x; i ++) {
+                    for (int j = 0; j <= y; j ++) {
+                        cowstatus [i] [j] =! cowstatus [i] [j];
+                    }
+                }
+                ans ++;
+            }
+        }
+    }
+    fout << ans;
+    return 0;
+}
